80bb8bcfd2c1aba8972fce3539b1575ac8090a5f
pmunusami <puratchidasan.munusami@am.sony.com>
2017-10-12 17:08:07 +0530

    Testing docker in GITHUB
    Testing docker in GITHUB

---
+++ dockerDemo/src/main/java/com/example/demo/DockerDemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tavant.spring.boot.SpringBootWebApplication;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(DockerDemoApplication.class);
+
+	@RequestMapping("/")
+    public String home() {
+		System.out.println("Serving client request...");
+		log.info("Logging the request");
+		
+        return "Hello Docker World "+Math.random()*10000+1;
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+}
